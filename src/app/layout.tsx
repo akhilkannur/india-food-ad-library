e252cc4ece468b650c50f55fdf2998ea887ecd59
akhilkannur <akhil.1000what@gmail.com>
2026-09-07 10:36:04 +0530

    Add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./base.css";
 import "./product.css";
 
@@ -11,6 +12,13 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body className="product-body">{children}</body>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RS0TMEM6GG" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-RS0TMEM6GG');`}
+      </Script>
     </html>
   );
 }
